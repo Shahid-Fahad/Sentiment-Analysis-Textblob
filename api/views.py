88ccf9cd97cc85b88fbed619bd0f8533f42a9c44
shzfahad <viperfahad@gmail.com>
2023-06-25 19:30:24 +0600

    Final Commit

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from setfit import SetFitModel
 from django.http import JsonResponse
 from django.http import HttpResponseRedirect
+from textblob import TextBlob
 
 # Create your views here.
 def f2(request):
@@ -13,20 +13,15 @@ def f2(request):
         return render(request, 'home.html')
 
 def res(request,anatext):
-        # Initialize and load the SetFit model
-        model = SetFitModel.from_pretrained("StatsGary/setfit-ft-sentinent-eval")
-        # Run inference on the input text
-        preds = model([anatext])
-        # Process the predictions as needed
-        sentiment_tensor = preds[0]  # Assuming the model predicts sentiment
-        
+        blob = TextBlob(anatext)
+        sentiment_tensor = blob.sentiment.polarity
         #labelling sentiment_tensor for meaningfull outcomes
-        sentiment="Neutral 🙂"
-        if sentiment_tensor==1:
+        if sentiment_tensor>0:
             sentiment="Positive 😁"
-        else:
+        elif sentiment_tensor<0:
             sentiment="Negative 😟"
-        
+        else:
+            sentiment="Neutral 🙂"
         # Render the results in a template
         return render(request, 'results.html', {'sentiment': sentiment})
 
